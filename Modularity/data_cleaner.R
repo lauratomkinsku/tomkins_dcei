c209181790f29f4eefb40930adca67d5aa871870
Laura Tomkins <lauratomkins@ku.edu>
2018-03-11 13:08:29 -0500

    3/11 commit, ready to create Rmarkdown

--- Modularity/data_cleaner.R
+++ Modularity/data_cleaner.R
@@ -1,11 +1,16 @@
 data_cleaner <- function(dat, nrecords){
   
+  # Check if there's any janky data locations
+  datbyyear <- aggregate(dat$year, by <- list(dat$name,dat$lon,dat$lat), FUN=length)
+  years <- unique(dat$year)
+  badstations <- datbyyear$Group.1[datbyyear$x!=length(years)]
+  dat <- subset(dat, !(dat$name %in% badstations))
+  
   # Get unique data
   uniquedat <- unique(dat[c('name','lat','lon')])
   stationnames <- uniquedat$name
   latlist <- uniquedat$lat
   lonlist <- uniquedat$lon
-  years <- unique(dat$year)
   
   # Put data into matrix
   cleandata <- matrix(data=dat$data,nrow=length(stationnames),ncol=length(years),byrow=TRUE)
--- Modularity/modularityhw.R
+++ Modularity/modularityhw.R
@@ -1,5 +1,4 @@
 # Modularity Homework
-source('data_cleaner.R')
 
 # Data Chunk
 provided <- TRUE
@@ -7,8 +6,13 @@ nrecords <- 40
 
 if (provided==TRUE){
   precip <- readRDS("USAAnnualPcpn1950_2008.rds")
-  temp <- readRDS("USAAnnualTemp1950_2008.rds")
+  temp <- readRDS("USAAnnualTemp1950_2016.rds")
+} else {
+  # Insert code for getting data online
 }
+
+# Cleaning chunk
+source('data_cleaner.R')
 
 cleanprecip <- data_cleaner(precip,nrecords)
 cleantemp <- data_cleaner(temp,nrecords)
@@ -17,3 +21,16 @@ remove(temp)
 remove(precip)
 
 # Plotting Chunk
+library(ggplot2)
+library(ggthemes)
+library(ggmap)
+
+conus <- get_stamenmap(c(left=-125, bottom=25.75, right=-67, top=49), zoom = 5, maptype = 'toner-lite')
+
+gg <- ggmap(conus) + 
+  geom_point(data=cleanprecip, aes(x=lon,y=lat,color=`2008`),size=1,na.rm=TRUE)+
+  labs(title = '1950 Precipitation Data', x = 'Longitude', y = 'Latitude') + 
+  scale_color_gradient2(low='white', high='blue', na.value = 'white')
+gg
+
+
